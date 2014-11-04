8eec702cc4b70f08737eee74618eaf3892ae43b7
Dexter Gramfors <dexter.gramfors@gmail.com>
2014-11-04 16:25:39 +0100

    Implemented pollard's rho algorithm

--- src/se/dxtr/Main.java
+++ src/se/dxtr/Main.java
@@ -1,8 +1,12 @@
 package se.dxtr;
 
+import java.math.BigInteger;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        BigInteger dex = new BigInteger("9110123735000000000000000000000000000000000000000000000000000000000001");
+        System.out.println(PrimeUtils.pollardRho(dex));
+        System.out.println(dex);
     }
 }
--- src/se/dxtr/PrimeUtils.java
+++ src/se/dxtr/PrimeUtils.java
@@ -8,11 +8,25 @@ import java.math.BigInteger;
 public class PrimeUtils {
 
     public static BigInteger gcd(BigInteger a, BigInteger b){
-        if(b.equals("0")){
+        if(b.equals(BigInteger.ZERO)){
             return a;
         } else {
             return gcd(a, a.mod(b));
         }
+    }
+
+    public static BigInteger pollardRho(BigInteger n) {
+        BigInteger d = new BigInteger("1");
+        BigInteger x = new BigInteger("2");
+        BigInteger y;
+        while (d.equals(BigInteger.ONE)){
+            x = x.pow(2).add(BigInteger.ONE).mod(n);
+            y = x.pow(2).add(BigInteger.ONE).mod(n);
+            d = gcd(x.subtract(y).abs(), n);
+        }
+        if (d.equals(n))
+            return new BigInteger("-1");
+        return d;
     }
 
 
