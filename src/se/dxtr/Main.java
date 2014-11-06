09a01cc563803ac044f98d618eae0bfccb4d2085
skogsharald <ludvig.jansson@outlook.com>
2014-11-06 20:37:51 +0100

    Fixed formatting to the stupid format on the website

--- src/se/dxtr/Main.java
+++ src/se/dxtr/Main.java
@@ -1,16 +1,25 @@
 package se.dxtr;
 
+import java.io.*;
 import java.math.BigInteger;
-import java.util.ArrayList;
+import java.util.*;
 
 public class Main {
+    // Change this to the instance of Pollard-Rho running
+    private static final int CORE_NUMBER = 1;
+    // Change this to your name
+    private static final String GROUP_MEMBER = "ludjan";
 
     public static void main(String[] args) {
-        BigInteger dex = new BigInteger("9110123735000000000000000000000000000000000000000000000000000000000001");
 
-        for(long i = 0; i < 13; i++){
+        BigInteger personnummer = new BigInteger("9112232872");
+        BigInteger longPersonnummer = new BigInteger("9112232872000000000000000000000000000000000000000000000000000000000001");
+        int j = 0;
+        initialPrint(personnummer, j);
+        // Change the index for each core
+        for(long i = 0; i < 25; i++){
 
-            BigInteger number = dex.add((BigInteger.valueOf(i)));
+            BigInteger number = longPersonnummer.add((BigInteger.valueOf(i)));
             System.out.println("******** NEW NUMBER: " + number + " ********");
             ArrayList<BigInteger> factors = PrimeUtils.pollardRho(number);
             if(factors.size() == 1){
@@ -18,8 +27,21 @@ public class Main {
             } else {
                 System.out.println("The factors for " + number + " are " + factors.toString());
             }
+            LinkedHashMap<BigInteger, Integer> matrix = new LinkedHashMap<BigInteger, Integer>();
+            for(BigInteger factor : factors){
+                if(matrix.containsKey(factor)){
+                    int num = matrix.get(factor) + 1;
+                    matrix.put(factor, num);
+                } else {
+                    matrix.put(factor, 1);
+                }
+            }
+
+
+            writeToFile(matrix);
         }
-        BigInteger number = new BigInteger("1231242341234");
+
+
 
         //PrimeUtils.pollardRho(new BigInteger("25"));
         //System.out.println(PrimeUtils.helpPollardRho(new BigInteger("4")));
@@ -28,5 +50,36 @@ public class Main {
         //System.out.println(factors.toString());
         //System.out.println(PrimeUtils.pollardRho(dex));
         //System.out.println(dex);
+    }
+
+    private static void initialPrint(BigInteger personnummer, int j){
+        try {
+            PrintWriter writer = new PrintWriter(new BufferedWriter(new FileWriter(GROUP_MEMBER + "_output_core_"+String.valueOf(CORE_NUMBER)+".in", true)));
+            writer.println(personnummer + " " + j);
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+    private static void writeToFile(LinkedHashMap<BigInteger, Integer> matrix){
+        try {
+            PrintWriter writer = new PrintWriter(new BufferedWriter(new FileWriter(GROUP_MEMBER + "_output_core_"+String.valueOf(CORE_NUMBER)+".in", true)));
+            Iterator it = matrix.entrySet().iterator();
+            StringBuilder sb = new StringBuilder();
+            while(it.hasNext()){
+                Map.Entry pairs = (Map.Entry)it.next();
+                sb.append(pairs.getKey() + " " + pairs.getValue() + " ");
+                it.remove();
+            }
+            writer.println(sb.toString());
+            writer.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
